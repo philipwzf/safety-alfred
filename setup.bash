54d3ef85325d938923d296048e137fe58a77809b
Philip Wang <128077099+philipwzf@users.noreply.github.com>
2025-09-16 08:43:13 -0500

    Update setup.bash to include DISPLAY constant note
    # Added a comment to update DISPLAY constant in gen/constants.py

--- setup.bash
+++ setup.bash
@@ -5,3 +5,4 @@ export API_KEY="your_api_key_here"
 # Start Xvfb on display :99
 Xvfb :99 -screen 0 1024x768x24 -ac +extension GLX +extension RANDR +extension RENDER &
 export DISPLAY=:99
+# Then change DISPLAY constant value to the screen number (99 here) in gen/constants.py
